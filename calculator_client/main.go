d4d7f4a715dd79b6d486867e302b5ea9f228933b
Rodrigo Ribeiro <rodrigo.ribeiro@olxbr.com>
2023-10-20 19:34:02 -0300

    project sample

--- calculator_client/main.go
+++ calculator_client/main.go
@@ -3,15 +3,45 @@ package main
 import (
 	"calculator-service/calculatorpb"
 	"context"
+	"crypto/x509"
 	"fmt"
 	"log"
 
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/acm"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/credentials"
 )
 
 func main() {
-	conn, err := grpc.Dial("localhost:50052", grpc.WithTransportCredentials(insecure.NewCredentials()))
+
+	arn := "arn:aws:acm:us-east-1:073521391622:certificate/add3bf34-0b92-43bd-b8e3-6943dc29a713"
+	region := "us-east-1"
+	address := "calculatorserver.lab.olxbr.cloud:443"
+
+	ctx := context.Background()
+
+	cfg, err := config.LoadDefaultConfig(ctx, config.WithRegion(region))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	client := acm.NewFromConfig(cfg)
+	certificate, err := client.GetCertificate(ctx, &acm.GetCertificateInput{CertificateArn: &arn})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	pool := x509.NewCertPool()
+	pool.AppendCertsFromPEM([]byte(*certificate.Certificate))
+	creds := credentials.NewClientTLSFromCert(pool, "")
+
+	//conn, err := grpc.Dial("calculatorserver.lab.olxbr.cloud:443", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	conn, err := grpc.DialContext(ctx, address, []grpc.DialOption{
+		grpc.WithTransportCredentials(creds),
+	}...)
 
 	if err != nil {
 		log.Fatalf("error %v", err)
